c9cdbdfa0ee07f5bdf716667520478e7e4b10625
nayanpal777 <nayanpalmns@gmail.com>
2020-07-11 12:21:32 +0530

    Updated 11/07/2020

--- js/jquery.min.js
+++ js/jquery.min.js
@@ -1,16 +1,4 @@
-/*!
- * jQuery JavaScript Library v3.2.1
- * https://jquery.com/
- *
- * Includes Sizzle.js
- * https://sizzlejs.com/
- *
- * Copyright JS Foundation and other contributors
- * Released under the MIT license
- * https://jquery.org/license
- *
- * Date: 2017-03-20T18:59Z
- */
+
 ( function( global, factory ) {
 
 	"use strict";
